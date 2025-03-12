b4aaa97fb804cb00b1d11638ed868bc0628b859a
Chris Alfano <chris@jarv.us>
2025-03-12 17:58:38 -0400

    feat: add projects page

---
+++ pages/projects.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <ProjectsList />
+  </div>
+</template>
+
+<script setup lang="ts">
+// Page component for /projects route
+definePageMeta({
+  layout: 'default',
+  title: 'Projects - Code for Philly'
+})
+</script>
